fb5df09f50aa302e20a2f45108a80faf549b085a
Ozyugoo <stellaozy22@gmail.com>
2024-06-13 11:45:51 +0100

    update

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
   let iconic = document.getElementById("toggle");
+  let times = document.getElementById("toggleMenu");
+  let menu = document.querySelector(".menu");
 
   iconic.addEventListener("click", function () {
-    let menu = document.querySelector(".menu");
     menu.classList.toggle("active");
+    iconic.classList.toggle("hidden");
+    times.classList.toggle("active");
+  });
+
+  times.addEventListener("click", function () {
+    menu.classList.toggle("active");
+    iconic.classList.toggle("hidden");
+    times.classList.toggle("active");
   });
 });
