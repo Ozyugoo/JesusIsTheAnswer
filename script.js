7299c7ae71f904cfa2d06819290fb14361c05fb1
Ozyugoo <stellaozy22@gmail.com>
2024-06-18 13:13:55 +0100

    Update

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let iconic = document.getElementById("toggle");
-  let times = document.getElementById("toggleMenu");
-  let menu = document.querySelector(".menu");
+// document.addEventListener("DOMContentLoaded", function () {
+//   let iconic = document.getElementById("toggle");
+//   let times = document.getElementById("toggleMenu");
+//   let menu = document.querySelector(".menu");
 
-  iconic.addEventListener("click", function () {
-    menu.classList.toggle("active");
-    iconic.classList.toggle("hidden");
-    times.classList.toggle("active");
-  });
+//   iconic.addEventListener("click", function () {
+//     menu.classList.toggle("active");
+//     iconic.classList.toggle("hidden");
+//     times.classList.toggle("active");
+//   });
 
-  times.addEventListener("click", function () {
-    menu.classList.toggle("active");
-    iconic.classList.toggle("hidden");
-    times.classList.toggle("active");
+//   times.addEventListener("click", function () {
+//     menu.classList.toggle("active");
+//     iconic.classList.toggle("hidden");
+//     times.classList.toggle("active");
+//   });
+// });
+
+function toggleSideBar() {
+  let sidebar = document.getElementById("sideBarModal");
+  if (sidebar.style.display === "block") {
+    sidebar.style.display = "none";
+  } else {
+    sidebar.style.display = "block";
+  }
+}
+
+let sideBarLinks = document.querySelectorAll(".sideBar-modal a");
+sideBarLinks.forEach(function (link) {
+  link.addEventListener("click", function () {
+    document.getElementById("sideBarModal").style.display = "none";
   });
 });
